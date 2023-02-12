c4dcd1c2f4f4599f42a70b2311a79012a3b11170
Lab-1 <saifhammad411@gmail.com>
2023-02-12 14:05:55 +0500

    cyclic

---
+++ we/src/App.jsx
@@ -0,0 +1,18 @@
+ import { useState } from "react"
+import axios from "axios";
+import './App.css';
+
+let baseUrl = ``;
+if (window.location.href.split(":")[0] === "http") {
+    baseUrl = `http://localhost:5001`;
+}
+
+function App() {
+    
+        }
+   
+
+export default App;
+
+
+
